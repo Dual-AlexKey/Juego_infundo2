ad866613f882a9e185ed16cd839441dec2ab0c86
Dual-AlexKey <almissann@gmail.com>
2024-09-24 16:06:16 -0500

    enviado

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ var flechaSound = document.getElementById('flechaSound');
 
 document.getElementById('flecha').addEventListener('click', function() {
     flechaSound.currentTime = 0;
-    flechaSound.volume = 0.4;
+    flechaSound.volume = 0.1;
     flechaSound.play();
 
-    const bloque2 = document.querySelector('.bloque2');
     const bloque1 = document.querySelector('.bloque1');
-
+    const bloque2 = document.querySelector('.bloque2');
 
     if (bloque1.style.display === 'block') {
-        bloque2.style.display = 'block';
         bloque1.style.display = 'none';
+        bloque2.style.display = 'block';
         document.getElementById('flecha').style.transform = 'rotate(180deg)';
     } else {
-        bloque2.style.display = 'none';
         bloque1.style.display = 'block';
+        bloque2.style.display = 'none';
         document.getElementById('flecha').style.transform = 'rotate(0deg)';
     }
 });
@@ -28,22 +27,6 @@ function reproducirSonidoHover() {
     sonidoHover.play();
 }
 
-// Event listeners para el primer grupo de botones
-document.getElementById('btn1').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn2').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn3').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn4').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn5').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn6').addEventListener('mouseover', reproducirSonidoHover);
-
-// Event listeners para el segundo grupo de botones
-document.getElementById('btn7').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn8').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn9').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn10').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn11').addEventListener('mouseover', reproducirSonidoHover);
-document.getElementById('btn12').addEventListener('mouseover', reproducirSonidoHover);
-
 // Función para manejar el redireccionamiento de los botones
 function handleModule(event) {
     const modulo = event.target.dataset.module; // Obtener el número de módulo del botón
@@ -51,23 +34,12 @@ function handleModule(event) {
     window.location.href = `./modulo${moduloEnlace}/modulo${moduloEnlace}.html`; // Redirigir al módulo correspondiente
 }
 
-// Event listeners para los botones del primer grupo
-document.getElementById('btn1').addEventListener('click', handleModule);
-document.getElementById('btn2').addEventListener('click', handleModule);
-document.getElementById('btn3').addEventListener('click', handleModule);
-document.getElementById('btn4').addEventListener('click', handleModule);
-document.getElementById('btn5').addEventListener('click', handleModule);
-document.getElementById('btn6').addEventListener('click', handleModule);
+const botones = document.querySelectorAll('.animated-button');
 
-// Event listeners para los botones del segundo grupo
-document.getElementById('btn7').addEventListener('click', handleModule);
-document.getElementById('btn8').addEventListener('click', handleModule);
-document.getElementById('btn9').addEventListener('click', handleModule);
-document.getElementById('btn10').addEventListener('click', handleModule);
-document.getElementById('btn11').addEventListener('click', handleModule);
-document.getElementById('btn12').addEventListener('click', handleModule);
-
+botones.forEach(boton => {
+    boton.addEventListener('mouseover', reproducirSonidoHover);
+    boton.addEventListener('click', handleModule);
+});
 
 var audio = document.getElementById('audioElement');
 audio.volume = 0.3;
-audio.play();
