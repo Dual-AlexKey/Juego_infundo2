800e839d9cbf9c4c1a0e2e59001a8bfe5bdd284f
Dual-AlexKey <almissann@gmail.com>
2024-10-15 14:26:33 -0500

    Actualizado

--- modulo1/juego1/script.js
+++ modulo1/juego1/script.js
@@ -30,6 +30,29 @@ const maxAttempts = 4;
 
 soundIcon.addEventListener('click', toggleSound);
 muteIcon.addEventListener('click', toggleSound);
+//modals
+var modal2 = document.getElementById('miModal');
+modal2.style.display = 'block';
+const modal = document.getElementById('miModal');
+const iconoAyuda = document.getElementById('abrirModal');
+
+// Obtener el botón para cerrar el modal
+const btnCerrarModal = document.getElementsByClassName('cerrar')[0];
+iconoAyuda.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+  
+  // Función para cerrar el modal al hacer clic en el botón de cerrar
+  btnCerrarModal.addEventListener('click', () => {
+    modal.style.display = 'none';
+  });
+  window.addEventListener('click', (evento) => {
+    if (evento.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+//fin modals
+  
 
 cards.forEach(card => {
     card.addEventListener('click', selectCard);
